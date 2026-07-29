e11ee557f11ce36bd2e33921b796482bd6502a13
Bikash Kumar Gouda <117624245+Bikashkumargouda@users.noreply.github.com>
2026-07-29 08:50:32 +0530

    Update API URL to use environment variable

--- src/components/TrainingForm.jsx
+++ src/components/TrainingForm.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const API = "http://localhost:5000/api";
+// const API = "http://localhost:5000/api";
+const API = import.meta.env.VITE_API_URL;
 
 function TrainingForm() {
   const [contractors, setContractors] = useState([]);
